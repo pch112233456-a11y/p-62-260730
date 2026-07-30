0e43621af6b95c9542621c83fc502b1bb8af7430
ChangHyeon Park <pch112233456@gmail.com>
2026-07-30 11:15:34 +0900

    first commit

---
+++ src/main/java/com/ddd/p62260730/P62260730Application.java
@@ -0,0 +1,13 @@
+package com.ddd.p62260730;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P62260730Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P62260730Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddd/p62260730/P62260730ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddd.p62260730;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P62260730ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
